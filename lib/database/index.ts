5944f31248f942a8cd0a85b0006b1c4677c7002b
BETSELOT ABRAHAM <138872691+whiHak@users.noreply.github.com>
2024-03-01 22:01:46 +0300

    ....

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -10,7 +10,7 @@ export const connectToDatabase = async() =>  {
     if(!MONGODB_URI) throw new Error("Mongodb URI Missing!!!");
 
     cached.promise = cached.promise || mongoose.connect(MONGODB_URI , {
-        dbName: "eventlys",
+        dbName: "evently",
         bufferCommands: false,
     })
 
