3d5032a846b71a396c04f424eac79f71f3b71bcc
BETSELOT ABRAHAM <138872691+whiHak@users.noreply.github.com>
2024-03-01 12:00:14 +0300

    ...

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export default authMiddleware({
     "/api/webhook/clerk",
     "/api/webhook/strip",
     "/api/uploadthing",
-    "/api/webhooks(.*)",
+    "/api/webhooks/clerk(.*)",
   ],
   // Routes that can always be accessed, and have
   // no authentication information
